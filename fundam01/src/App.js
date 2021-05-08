2ecfa3f595256bd81899b0fcf94b9480f40dca36
hardler10 <fabricio.santos188@etec.sp.gov.br>
2021-05-08 11:33:01 +0000

    1componente

---
+++ fundam01/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Aula 01 - Fundamentos 01</h1>
+        
+        <OlaMundo />
+
+      </div>
+    )
+  }
+}
+
+export default App;
